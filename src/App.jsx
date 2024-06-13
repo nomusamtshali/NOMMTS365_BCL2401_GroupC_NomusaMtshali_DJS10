06c6efa4df517b1ab8c221c4846069d219a23c93
Nomusa Mtshali <kelebogilemataboge17@gmail.com git config --global user.email kelebogilemataboge17@gmail.com>
2024-06-13 14:48:04 +0200

    used the 'useEffect' hook to fetch data from the provided API and threw an error

---
+++ src/App.jsx
@@ -0,0 +1,31 @@
+// import React from "react"
+import { useEffect, useState } from "react";
+
+export default function App() {
+  const [posts, setPosts] = useState([]);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    const fetchPosts = async () => {
+      try {
+        const response = await fetch(
+          "https://jsonplaceholder.typicode.com/posts"
+        );
+        if (!response.ok) {
+          throw new Error(`Data fetching failed`);
+        }
+        const data = await response.json();
+        setPosts(data);
+      } catch (error) {
+        setError(error.message);
+      }
+    };
+
+    fetchPosts();
+  }, []);
+
+  return (
+    <>
+    </>
+  );
+}
